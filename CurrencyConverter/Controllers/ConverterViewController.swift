e47894789796b5adcf7f08d736ff8011d45c0fbd
Ermolenko.K <19symbols@gmail.com>
2021-02-16 19:26:12 +0300

    Добавил кнопки для валют

---
+++ CurrencyConverter/Controllers/ConverterViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ConverterViewController.swift
+//  CurrencyConverter
+//
+//  Created by Ермоленко Константин on 16.02.2021.
+//
+
+import UIKit
+
+class ConverterViewController: UIViewController {
+    
+    var firstCurrency: UIButton!
+    var secondCurrency: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+    }
+    
+    func configure() {
+        title = "Converter"
+        view.backgroundColor = .systemBackground
+        layoutElements()
+    }
+    
+    func layoutElements() {
+        firstCurrency = UIButton()
+        firstCurrency.backgroundColor = .systemPink
+        firstCurrency.translatesAutoresizingMaskIntoConstraints = false
+        firstCurrency.accessibilityIdentifier = "firstCurrency"
+        firstCurrency.setTitle("USD", for: .normal)
+        
+        secondCurrency = UIButton()
+        secondCurrency.backgroundColor = .systemTeal
+        secondCurrency.translatesAutoresizingMaskIntoConstraints = false
+        secondCurrency.accessibilityIdentifier = "secondCurrency"
+        secondCurrency.setTitle("RUB", for: .normal)
+
+        view.addSubview(firstCurrency)
+        view.addSubview(secondCurrency)
+
+        firstCurrency.backgroundColor = .systemTeal
+        secondCurrency.backgroundColor = .systemPink
+        
+        let padding: CGFloat = 10
+        let safeArea = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            firstCurrency.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: padding),
+            firstCurrency.heightAnchor.constraint(equalToConstant: 100),
+            firstCurrency.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: padding),
+            firstCurrency.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -padding),
+
+            secondCurrency.topAnchor.constraint(equalTo: firstCurrency.bottomAnchor, constant: padding),
+            secondCurrency.heightAnchor.constraint(equalToConstant: 100),
+            secondCurrency.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: padding),
+            secondCurrency.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -padding),
+        ])
+    }
+}
--- CurrencyConverterUITests/CurrencyConverterUITests.swift
+++ CurrencyConverterUITests/CurrencyConverterUITests.swift
@@ -1,13 +1,13 @@
 //
-//  CurrencyConverterUITests.swift
-//  CurrencyConverterUITests
+//  ConverterViewControllerUITests.swift
+//  ConverterViewControllerUITests
 //
 //  Created by Ермоленко Константин on 16.02.2021.
 //
 
 import XCTest
 
-class CurrencyConverterUITests: XCTestCase {
+class ConverterViewControllerUITests: XCTestCase {
     
     var app: XCUIApplication!
 
@@ -18,8 +18,18 @@ class CurrencyConverterUITests: XCTestCase {
         app.launch()
     }
 
-    func testStartingScreenIsConverter() throws {
+    func testConverterIsStartingScreen() throws {
         let title = app.navigationBars["Converter"]
         XCTAssertTrue(title.exists)
+    }
+    
+    func testTwoCurrencyLabelExist() throws {
+        let firstCurrency = app.buttons["firstCurrency"]
+        XCTAssertTrue(firstCurrency.exists)
+        XCTAssertEqual(firstCurrency.label, "USD", "First currency should be USD")
+                
+        let secondCurrency = app.buttons["secondCurrency"]
+        XCTAssertTrue(secondCurrency.exists)
+        XCTAssertEqual(secondCurrency.label, "RUB", "Second currency should be RUB")
     }
 }
