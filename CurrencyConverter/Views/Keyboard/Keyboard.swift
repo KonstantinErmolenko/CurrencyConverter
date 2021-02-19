a7c8d0523001f6c596fdf9baab1f58957a214a4a
Ermolenko.K <19symbols@gmail.com>
2021-02-19 14:29:34 +0300

    Рефакторинг создания кнопок клавиатуры. Новые тесты проверки клавиатуры.
    В новом тесте проверяется горизонтальное и вертикальное расположение кнопок

--- CurrencyConverter/Views/Keyboard/Keyboard.swift
+++ CurrencyConverter/Views/Keyboard/Keyboard.swift
@@ -24,42 +24,36 @@ class Keyboard: UIView {
         accessibilityIdentifier = "keyboard"
         backgroundColor = Colors.swatch3
         translatesAutoresizingMaskIntoConstraints = false
+        configureStackView()
         layoutElements()
     }
     
-    func layoutElements() {
+    private func layoutElements() {
+        addSubview(stackView)
+        stackView.pinToEdges(of: self)
+    }
+
+    private func configureStackView() {
         stackView = UIStackView()
         stackView.backgroundColor = Colors.swatch3
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.spacing = 2
         stackView.axis = .vertical
         stackView.distribution = .fillEqually
-        addSubview(stackView)
         
-        let row1 = KeyboardRow()
-        for number in 1...3 {
-            row1.addArrangedSubview(KeyboardNumberButton(number: number))
+        let numbers = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        for numbersInRow in numbers {
+            let row = KeyboardRow()
+            for number in numbersInRow {
+                row.addArrangedSubview(KeyboardNumberButton(number: number))
+            }
+            stackView.addArrangedSubview(row)
         }
-        stackView.addArrangedSubview(row1)
-        
-        let row2 = KeyboardRow()
-        for number in 4...6 {
-            row2.addArrangedSubview(KeyboardNumberButton(number: number))
-        }
-        stackView.addArrangedSubview(row2)
 
-        let row3 = KeyboardRow()
-        for number in 7...9 {
-            row3.addArrangedSubview(KeyboardNumberButton(number: number))
-        }
-        stackView.addArrangedSubview(row3)
-
-        let row4 = KeyboardRow()
-        row4.addArrangedSubview(KeyboardActionButton(action: .comma))
-        row4.addArrangedSubview(KeyboardNumberButton(number: 0))
-        row4.addArrangedSubview(KeyboardActionButton(action: .delete))
-        stackView.addArrangedSubview(row4)
-
-        stackView.pinToEdges(of: self)
+        let row = KeyboardRow()
+        row.addArrangedSubview(KeyboardActionButton(action: .comma))
+        row.addArrangedSubview(KeyboardNumberButton(number: 0))
+        row.addArrangedSubview(KeyboardActionButton(action: .delete))
+        stackView.addArrangedSubview(row)
     }
 }
