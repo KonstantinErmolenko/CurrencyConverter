c67f7923681943b8ee5ff3445195fd1442763d03
Ermolenko.K <19symbols@gmail.com>
2021-02-20 19:53:21 +0300

    Рефакторинг UI тестов

--- CurrencyConverterUITests/CurrencyConverterUITests.swift
+++ CurrencyConverterUITests/CurrencyConverterUITests.swift
@@ -23,19 +23,11 @@ class ConverterViewControllerUITests: XCTestCase {
         XCTAssertTrue(title.exists)
     }
     
-    func testCurrencyButtonsHaveCorrectTitles() throws {
-        let firstCurrency = app.buttons["currencyFrom"]
-        XCTAssertEqual(firstCurrency.label, "USD", "First currency should be USD")
-                
-        let secondCurrency = app.buttons["currencyTo"]
-        XCTAssertEqual(secondCurrency.label, "RUB", "Second currency should be RUB")
-    }
-    
     func testCurrencyButtonsAreConfiguredCorrectly() throws {
-        let firstCurrency = app.buttons["firstCurrency"]
-        XCTAssertTrue(firstCurrency.exists)
-        let secondCurrency = app.buttons["currencyTo"]
-        XCTAssertTrue(secondCurrency.exists)
+        let currencyFrom = app.buttons["currencyFrom"]
+        XCTAssertTrue(currencyFrom.exists)
+        let currencyTo = app.buttons["currencyTo"]
+        XCTAssertTrue(currencyTo.exists)
     }
 
     func testAccessoryPanelConfiguredCorrectly() throws {
@@ -47,9 +39,14 @@ class ConverterViewControllerUITests: XCTestCase {
         XCTAssertTrue(button2.exists, "The accessory panel must have a Swop button")
         let button3 = app.buttons["nextButton"]
         XCTAssertTrue(button3.exists, "The accessory panel must have a Next button")
+                
+        XCTAssertTrue(
+            horizontalPositionOfButtons(looksLike: "previousButton", "swapButton", "nextButton"),
+            "The accessory panel should look like previous, swap, next"
+        )
     }
    
-    func testAllKeyboardButtonsIsHittable() throws {
+    func testAllKeyboardButtonsAreHittable() throws {
         for number in 0...9 {
             let numberButton = app.buttons["numberButton\(number)"]
             XCTAssertTrue(
@@ -76,68 +73,68 @@ class ConverterViewControllerUITests: XCTestCase {
         XCTAssertTrue(keyboard.exists)
 
         XCTAssertTrue(
-            horizontalPositionIsCorrect("numberButton1", "numberButton2", "numberButton3"),
+            horizontalPositionOfButtons (looksLike: "numberButton1", "numberButton2", "numberButton3"),
             "The first row should look like 1, 2, 3"
         )
         XCTAssertTrue(
-            horizontalPositionIsCorrect("numberButton4", "numberButton5", "numberButton6"),
+            horizontalPositionOfButtons (looksLike: "numberButton4", "numberButton5", "numberButton6"),
             "The second row should look like 4, 5, 6"
         )
         XCTAssertTrue(
-            horizontalPositionIsCorrect("numberButton7", "numberButton8", "numberButton9"),
+            horizontalPositionOfButtons (looksLike: "numberButton7", "numberButton8", "numberButton9"),
             "The third row should look like 7, 8, 9"
         )
         XCTAssertTrue(
-            horizontalPositionIsCorrect("commaButton", "numberButton0", "deleteButton"),
+            horizontalPositionOfButtons (looksLike: "commaButton", "numberButton0", "deleteButton"),
             "The fourth row should look like comma, 0, delete"
         )
         
         XCTAssertTrue(
-            verticalPositionIsCorrect("numberButton1",
-                                      "numberButton4",
-                                      "numberButton7",
-                                      "commaButton"),
+            verticalPositionOfButtons(looksLike: "numberButton1",
+                                                  "numberButton4",
+                                                  "numberButton7",
+                                                  "commaButton"),
             "The first column should look like 1, 4, 7, comma"
         )
         XCTAssertTrue(
-            verticalPositionIsCorrect("numberButton2",
-                                      "numberButton5",
-                                      "numberButton8",
-                                      "numberButton0"),
+            verticalPositionOfButtons(looksLike: "numberButton2",
+                                                  "numberButton5",
+                                                  "numberButton8",
+                                                  "numberButton0"),
             "The second column should look like 2, 5, 8, 0"
         )
         XCTAssertTrue(
-            verticalPositionIsCorrect("numberButton3",
-                                      "numberButton6",
-                                      "numberButton9",
-                                      "deleteButton"),
+            verticalPositionOfButtons(looksLike: "numberButton3",
+                                                  "numberButton6",
+                                                  "numberButton9",
+                                                  "deleteButton"),
             "The third column should look like 3, 6, 9, delete"
         )
     }
     
-    func horizontalPositionIsCorrect (_ button1: String,
-                                      _ button2: String,
-                                      _ button3: String) -> Bool {
-        let button1 = app.buttons[button1].frame.midX
-        let button2 = app.buttons[button2].frame.midX
-        let button3 = app.buttons[button3].frame.midX
-        
-        return button1 < button2 && button2 < button3
+    private func horizontalPositionOfButtons (looksLike buttons: String...) -> Bool {
+        var coordinates = [CGFloat]()
+        for button in buttons {
+            coordinates.append(app.buttons[button].frame.midX)
+        }
+
+        return coordinateSequenceIsCorrect(coordinates: coordinates)
     }
     
-    func verticalPositionIsCorrect (_ button1: String,
-                                    _ button2: String,
-                                    _ button3: String,
-                                    _ button4: String) -> Bool {
-        let button1 = app.buttons[button1].frame.midY
-        let button2 = app.buttons[button2].frame.midY
-        let button3 = app.buttons[button3].frame.midY
-        let button4 = app.buttons[button4].frame.midY
+    private func verticalPositionOfButtons (looksLike buttons: String...) -> Bool {
+        var coordinates = [CGFloat]()
+        for button in buttons {
+            coordinates.append(app.buttons[button].frame.midY)
+        }
 
-        let result = button1 < button2
-                     && button2 < button3
-                     && button3 < button4
+        return coordinateSequenceIsCorrect(coordinates: coordinates)
+    }
+    
+    private func coordinateSequenceIsCorrect(coordinates: [CGFloat]) -> Bool {
+        let incorrect = stride(from: 0, to: coordinates.count - 1, by: 1)
+                        .map { coordinates[$0] < coordinates[$0 + 1] }
+                        .contains(false)
         
-        return result
+        return !incorrect
     }
 }
